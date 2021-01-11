9a6b612c08c58a128f800d8c491d996573b80ec1
yujo <yujo@student.42seoul.kr>
2021-01-11 20:50:14 +0900

    refactor: make clean code

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,49 +2,6 @@ import { authService, firebaseInstance } from 'fbase';
 import React, { useState } from 'react';
 
 const Auth = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState('');
-
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-
-    if (name === 'email') {
-      setEmail(value);
-    } else if (name === 'password') {
-      setPassword(value);
-    }
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      if (newAccount) {
-        const data = await authService.createUserWithEmailAndPassword(
-          email,
-          password,
-        );
-        console.log(data);
-      } else {
-        const data = await authService.signInWithEmailAndPassword(
-          email,
-          password,
-        );
-        console.log(data);
-      }
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const toggleAccout = () => {
-    setNewAccount((prev) => !prev);
-  };
-
   const onSocialClick = async (event) => {
     const {
       target: { name },
@@ -62,32 +19,6 @@ const Auth = () => {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Passwod"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input
-          type="submit"
-          value={newAccount ? 'Create Account' : 'Sign In'}
-        />
-        {error}
-      </form>
-      <span onClick={toggleAccout}>
-        {newAccount ? 'Sign In' : 'Create Account'}
-      </span>
       <div>
         <button onClick={onSocialClick} name="google">
           Continue with Google
